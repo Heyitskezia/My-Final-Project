a3e1f2c746aae7268089d7c389e69e57bfcecf90
Heyitskezia <Nathania98@gmail.com>
2016-10-21 17:31:28 +0700

    Final Project (Reservation System).cpp

---
+++ Final Project (Reservation System).cpp
@@ -0,0 +1,43 @@
+// Final Project (Reservation System)
+// Name: Nathania Kezia Barakati
+// ID: 2001586331
+// Major/Batch: Computer Science / 2020
+// Sorry for the little update. :/
+
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <cstdio>
+using namespace std;
+
+static int a = 0;
+class b
+
+{
+	public:
+	char album[20], artist[10], release[4], genre[10];
+	void install();
+	void show();
+	
+}
+
+{
+  cout<<"\nAlbum Name: ";
+  cin>>album[a].album;
+
+  cout<<"\nArtist: ";
+  cin>>album[a].artist;
+
+  cout<<"\nRelease Time: ";
+  cin>>album[a].release;
+  
+  cout<<"\nGenre: ";
+  cin>>album[a].genre;
+}
+
+
+
+
+
+
+
